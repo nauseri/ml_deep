2893bad77b86427fa7de45601930c2ee97fef241
nauseri <nauseri@student.ethz.ch>
2019-11-26 16:42:04 +0100

    last deep chp added

---
+++ deep12.py
@@ -0,0 +1,37 @@
+
+# ml mastery ebook: deep learning w python Appendix A, B and C
+
+# Appendix A
+# Getting More Help
+
+# links on...
+# Artiﬁcial Neural Networks
+# Deep Learning
+# Python Machine Learning
+# Keras Library
+# The Keras blog provides updates and tutorials on the platform.
+# http://blog.keras.io
+# The best place to get answers to your Keras questions is on the Google group email list.
+# https://groups.google.com/forum/#!forum/keras-users
+# A good secondary place to ask questions is Stack Overﬂow and use the Keras tag with
+# your question.
+# http://stackoverflow.com/questions/tagged/keras
+
+
+# Appendix B
+# how to install python w conda, libraries, further reading...
+
+
+# Appendix C
+# How to Setup Amazon EC2 for Deep Learning on remote, paid GPUs
+
+
+
+
+
+
+
+
+
+
+
